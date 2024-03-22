be69a121216762e11484980a90def0c9274960c7
JATIN SAREEN <102468240+jatin-12-2002@users.noreply.github.com>
2024-03-22 20:48:37 +0000

    setup updated

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Chest-Disease-Classification-from-Chest-CT-Scan-Image"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "jatin-12-2002"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "sareenjatin002@gmail.com"
 
 
 setuptools.setup(
